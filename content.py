adc2c99646020c7875e3a45619faf12aec36c8b5
Jeremiah <jlsnow301@pm.me>
2022-08-23 21:57:09 +0000

    add code in files types: .json,.lock remove code in files types: .json,.lock

---
+++ content.py
@@ -0,0 +1,32 @@
+# Contributions-Importer-For-Github/run_script.py
+import git
+import os
+from git_contributions_importer import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# repos
+web = os.getenv("WEB_REPO")
+api = os.getenv("API_REPO")
+aws = os.getenv("AWS_REPO")
+sandbox = os.getenv("SANDBOX_REPO")
+mock = os.getenv("MOCK_REPO")
+
+mock_repo = git.Repo(mock)
+
+# emails
+email1 = os.getenv("PERSONAL_EMAIL1")
+email2 = os.getenv("PERSONAL_EMAIL2")
+email3 = os.getenv("WORK_EMAIL")
+email4 = os.getenv("GITHUB_EMAIL")
+
+# change cwd to projects folder
+os.chdir(os.getenv("PROJECTS_FOLDER"))
+
+for repo in [web, api, aws, sandbox]:
+    private_repo = git.Repo(repo)
+    importer = Importer([private_repo], mock_repo)
+    importer.set_author([email1, email2, email3, email4])
+    importer.import_repository()
